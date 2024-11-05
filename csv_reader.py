0d4547f074330d1ea946bcbf8711e66664ce1fd5
marce <marcel.senn@students.fhnw.ch>
2024-11-05 15:39:51 +0100

    05.11 csv reader updated

--- csv_reader.py
+++ csv_reader.py
@@ -1,7 +1,7 @@
 # This program reads test scores from a CSV file
 # and calculates each student's test average.
 from matplotlib import lines
-
+import re
 
 def main():
     # Open the file.
@@ -14,10 +14,20 @@ def main():
     csv_file.close()
 
     # Process the lines.
+    #for line in lines[1:]:
     for line in lines:
-        # Get the test scores as tokens.
-        tokens = line.split(',')
-        print(tokens)
+        # Get the book as tokens.
+        modified_line = re.sub(r',(?=\S)', '|', line)
+
+        book= modified_line.split('|')
+
+        isbn = book[6]
+        print(isbn)
+
+
+        # get the isbn
+        #for attributes in book:
+        #    print(attributes)
 
 
 
