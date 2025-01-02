9f3e5912e299b8788206ab2d7da9af13702edb56
KarGsch <146237826+KarGsch@users.noreply.github.com>
2025-01-02 15:39:59 +0100

    minor changes to displayed information

--- pages/SelectorScreen.py
+++ pages/SelectorScreen.py
@@ -18,9 +18,8 @@ if "book_remove" in st.session_state:
 if len(st.session_state.book_comparisons) < 2:
     st.switch_page(winner_screen)
 
-st.title("2 Books from Book List:")
-st.write("Welcome to the Selector Screen!")
-st.write("Which book would you like to read?")
+st.title("Welcome to the Selector Screen!")
+st.write("Which book would you like to read more?")
 
 # Create two columns to display the books side by side
 col1, col2 = st.columns(2)
@@ -28,50 +27,62 @@ col1, col2 = st.columns(2)
 # Randomly select two books
 book1, book2 = random.sample(st.session_state.book_comparisons, 2)
 
-with col1:
-    st.header(book1.title)
-    st.image(book1.cover_url['thumbnail'])
-    #if this button is pressed, remove the other displayed book
-    st.button(book1.title, args=[book2], on_click=book_clicked)
 
+with col1:
+    # get the full title of the book and store it in a string variable
+    book1_title = book1.title
+
+    # Check if the title contains brackets and extract the substring
+    # e.g. "Flames of Chaos (Legacy of the Nine Realms, #1)"
+    if '(' in book1_title and ')' in book1_title:
+        book1_title_only = book1_title.split('(')[0].strip()
+        series1 = book1_title.split('(')[1].split(')')[0].strip()
+    else:
+        book1_title_only = book1_title
+        series1 = "Not Applicable"
+
+    #if this button is pressed, remove the other displayed book
+    st.button(book1_title_only, args=[book2], on_click=book_clicked)
+
+    #display the book
+    st.header(book1_title_only)
+    st.image(book1.cover_url['thumbnail'], width=200)
+    st.write("")
     st.write(f"**Author:** {book1.author}")
-    st.write(f"**ISBN-10:** {book1.isbn10}")
-    st.write(f"**ISBN-13:** {book1.isbn13}")
-    st.write(f"**Year:** {book1.year}")
+    st.write(f"**Series:** {series1}")
+    st.write(f"**Year Published:** {book1.year}")
     st.write(f"**Page Count:** {book1.page_count}")
     st.write(f"**Description:** {book1.description}")
 
 with col2:
-    st.header(book2.title)
-    st.image(book2.cover_url['thumbnail'])
-    #if this button is pressed, remove the other displayed book
-    st.button(book2.title, args=[book1], on_click=book_clicked)
+    # get the full title of the book and store it in a string variable
+    book2_title = book2.title
 
+    # Check if the title contains brackets and extract the substring
+    # e.g. "Flames of Chaos (Legacy of the Nine Realms, #1)"
+    if '(' in book2_title and ')' in book2_title:
+        book2_title_only = book2_title.split('(')[0].strip()
+        series2 = book2_title.split('(')[1].split(')')[0].strip()
+    else:
+        book2_title_only = book2_title
+        series2 = "Not Applicable"
+
+    #if this button is pressed, remove the other displayed book
+    st.button(book2_title_only, args=[book1], on_click=book_clicked)
+
+    # display the book
+    st.header(book2_title_only)
+    st.image(book2.cover_url['thumbnail'], width=200)
     st.write("")
     st.write(f"**Author:** {book2.author}")
-    st.write(f"**ISBN-10:** {book2.isbn10}")
-    st.write(f"**ISBN-13:** {book2.isbn13}")
-    st.write(f"**Year:** {book2.year}")
+    st.write(f"**Series:** {series2}")
+    st.write(f"**Year Published:** {book2.year}")
     st.write(f"**Page Count:** {book2.page_count}")
     st.write(f"**Description:** {book2.description}")
 
-
-
-
-# implement logic to remove 1 book from book_comparisons before uncommenting this line
-# while len(st.session_state.book_comparisons) > 1:
-
-# def button_on_image(book, key):
-#     st.markdown(
-#         f"""
-#         <div style="position: relative; display: inline-block; text-align: center;">
-#             <img src="{book.cover_url['thumbnail']}" alt="{book.title}" style="width: 180px; height: auto;">
-#             <button style="position: absolute; top: 0%; left: 0%; width: 100%; height: 100%;
-#                  background: green; border: none; cursor: pointer;"
-#                  onclick=st.session_state.{key}>
-#             </button>
-#         </div>
-#         """,
-#         unsafe_allow_html=True
-#     )
-#     st.session_state.clicked = book
+# Handle the clicked book
+if "clicked_book" in st.session_state:
+    if st.session_state.clicked_book == book1:
+        book_clicked(book2)
+    elif st.session_state.clicked_book == book2:
+        book_clicked(book1)
